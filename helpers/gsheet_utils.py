fa72d224ceb5638e93e12c0732c3b91dd283d9fe
Kumar Jain <kumarjain@Kumars-MacBook-Air.local>
2025-07-26 00:33:16 +0530

    Updated gsheet_utils with secrets dict auth

--- helpers/gsheet_utils.py
+++ helpers/gsheet_utils.py
@@ -1,9 +1,10 @@
+import streamlit as st
 import gspread
 from oauth2client.service_account import ServiceAccountCredentials
-import streamlit as st
 
 def connect_sheet(sheet_name):
-    scope = ['https://spreadsheets.google.com/feeds', 'https://www.googleapis.com/auth/drive']
+    scope = ["https://spreadsheets.google.com/feeds", "https://www.googleapis.com/auth/drive"]
+    
     creds_dict = {
         "type": st.secrets["type"],
         "project_id": st.secrets["project_id"],
@@ -14,9 +15,9 @@ def connect_sheet(sheet_name):
         "auth_uri": st.secrets["auth_uri"],
         "token_uri": st.secrets["token_uri"],
         "auth_provider_x509_cert_url": st.secrets["auth_provider_x509_cert_url"],
-        "client_x509_cert_url": st.secrets["client_x509_cert_url"],
-        "universe_domain": st.secrets["universe_domain"]
+        "client_x509_cert_url": st.secrets["client_x509_cert_url"]
     }
+
     creds = ServiceAccountCredentials.from_json_keyfile_dict(creds_dict, scope)
     client = gspread.authorize(creds)
     return client.open(sheet_name)
