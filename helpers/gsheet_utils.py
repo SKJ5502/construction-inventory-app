0d7077a64ee8905eaf604fc3d28c025dd15ca5c6
Kumar Jain <kumarjain@Kumars-MacBook-Air.local>
2025-07-25 23:42:41 +0530

    Updated gsheet_utils.py to use Streamlit secrets

--- helpers/gsheet_utils.py
+++ helpers/gsheet_utils.py
@@ -1,8 +1,11 @@
+import streamlit as st
 import gspread
+import json
 from oauth2client.service_account import ServiceAccountCredentials
 
 def connect_sheet(sheet_name):
     scope = ["https://spreadsheets.google.com/feeds", "https://www.googleapis.com/auth/drive"]
-    creds = ServiceAccountCredentials.from_json_keyfile_name("gsheet_auth.json", scope)
+    gsheet_dict = json.loads(st.secrets["GSHEET_JSON"])
+    creds = ServiceAccountCredentials.from_json_keyfile_dict(gsheet_dict, scope)
     client = gspread.authorize(creds)
     return client.open(sheet_name)
