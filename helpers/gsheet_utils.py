34090c26b64b4096bd38ea0e032ec49168440880
Kumar Jain <kumarjain@Kumars-MacBook-Air.local>
2025-07-26 00:22:15 +0530

    Initial commit

--- helpers/gsheet_utils.py
+++ helpers/gsheet_utils.py
@@ -1,11 +1,22 @@
-import streamlit as st
 import gspread
-import json
 from oauth2client.service_account import ServiceAccountCredentials
+import streamlit as st
 
 def connect_sheet(sheet_name):
-    scope = ["https://spreadsheets.google.com/feeds", "https://www.googleapis.com/auth/drive"]
-    gsheet_dict = json.loads(st.secrets["GSHEET_JSON"])
-    creds = ServiceAccountCredentials.from_json_keyfile_dict(gsheet_dict, scope)
+    scope = ['https://spreadsheets.google.com/feeds', 'https://www.googleapis.com/auth/drive']
+    creds_dict = {
+        "type": st.secrets["type"],
+        "project_id": st.secrets["project_id"],
+        "private_key_id": st.secrets["private_key_id"],
+        "private_key": st.secrets["private_key"],
+        "client_email": st.secrets["client_email"],
+        "client_id": st.secrets["client_id"],
+        "auth_uri": st.secrets["auth_uri"],
+        "token_uri": st.secrets["token_uri"],
+        "auth_provider_x509_cert_url": st.secrets["auth_provider_x509_cert_url"],
+        "client_x509_cert_url": st.secrets["client_x509_cert_url"],
+        "universe_domain": st.secrets["universe_domain"]
+    }
+    creds = ServiceAccountCredentials.from_json_keyfile_dict(creds_dict, scope)
     client = gspread.authorize(creds)
     return client.open(sheet_name)
